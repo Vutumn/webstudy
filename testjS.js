58fc2fdc577dba8975b45364885c24b8d748da7c
x <314708369@qq.com>
2017-12-18 17:28:54 +0800

    Initial commit

---
+++ testjS.js
@@ -0,0 +1,13 @@
+<script>
+    function howMany(selectObject) {
+    var numberSelected = 0;
+    for (var i = 0; i < selectObject.options.length; i++){
+        if (selectObject.options[i].selected){
+          numberSelected++;
+         }
+      }
+    return numberSelected;
+}
+
+
+</script>
