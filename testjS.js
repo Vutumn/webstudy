5474c8c6b7adf7cd8c2a6f1689adca7768c6cdb5
x <314708369@qq.com>
2018-01-02 17:07:07 +0800

    header first finish

--- testjS.js
+++ testjS.js
@@ -1,13 +1,1 @@
-<script>
-    function howMany(selectObject) {
-    var numberSelected = 0;
-    for (var i = 0; i < selectObject.options.length; i++){
-        if (selectObject.options[i].selected){
-          numberSelected++;
-         }
-      }
-    return numberSelected;
-}
 
-
-</script>
